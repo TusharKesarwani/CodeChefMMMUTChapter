d118f5ed9828802ebf12834a2b919e399ba9f452
Tushar <tusharkesarwani44@gmail.com>
2022-08-31 20:57:41 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+VANTA.NET({
+  el: document.getElementById("animated_background"),
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  points: 14.00,
+  maxDistance: 22.00,
+  spacing: 16.00,
+  showDots: false
+})
