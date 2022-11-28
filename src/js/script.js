9aa526db5cdcfa37ec0bbf2368fcfb667e0f46b3
Tushar <tusharkesarwani44@gmail.com>
2022-11-28 17:49:57 +0530

    Edited preloader and font

--- src/js/script.js
+++ src/js/script.js
@@ -3,12 +3,13 @@ VANTA.NET({
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
-  minHeight: 200.0,
-  minWidth: 200.0,
-  scale: 1.0,
-  scaleMobile: 1.0,
-  points: 14.0,
-  maxDistance: 22.0,
-  spacing: 16.0,
-  showDots: false,
-});
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x3fbbff,
+  backgroundColor: 0xe18,
+  points: 11.00,
+  maxDistance: 22.00,
+  spacing: 18.00
+})
