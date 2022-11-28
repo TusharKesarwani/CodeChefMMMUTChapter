e541381e2e0b9807f204c526b7d506a71d8c9336
Tushar <tusharkesarwani44@gmail.com>
2022-11-28 17:01:07 +0530

    Revert back to previous front page

--- src/js/script.js
+++ src/js/script.js
@@ -1,57 +1,14 @@
-(function () {
-	var d = document,
-		accordionToggles = d.querySelectorAll(".js-accordionTrigger"),
-		setAccordionAria,
-		switchAccordion,
-		touchSupported = "ontouchstart" in window,
-		pointerSupported = "pointerdown" in window;
-
-	skipClickDelay = function (e) {
-		e.preventDefault();
-		e.target.click();
-	};
-
-	setAriaAttr = function (el, ariaType, newProperty) {
-		el.setAttribute(ariaType, newProperty);
-	};
-	setAccordionAria = function (el1, el2, expanded) {
-		switch (expanded) {
-			case "true":
-				setAriaAttr(el1, "aria-expanded", "true");
-				setAriaAttr(el2, "aria-hidden", "false");
-				break;
-			case "false":
-				setAriaAttr(el1, "aria-expanded", "false");
-				setAriaAttr(el2, "aria-hidden", "true");
-				break;
-			default:
-				break;
-		}
-	};
-
-	switchAccordion = function (e) {
-		e.preventDefault();
-		var thisAnswer = e.target.parentNode.nextElementSibling;
-		var thisQuestion = e.target;
-		if (thisAnswer.classList.contains("is-collapsed")) {
-			setAccordionAria(thisQuestion, thisAnswer, "true");
-		} else {
-			setAccordionAria(thisQuestion, thisAnswer, "false");
-		}
-		thisQuestion.classList.toggle("is-collapsed");
-		thisQuestion.classList.toggle("is-expanded");
-		thisAnswer.classList.toggle("is-collapsed");
-		thisAnswer.classList.toggle("is-expanded");
-
-		thisAnswer.classList.toggle("animateIn");
-	};
-	for (var i = 0, len = accordionToggles.length; i < len; i++) {
-		// if (touchSupported) {
-		// 	accordionToggles[i].addEventListener("touchstart", skipClickDelay, false);
-		// }
-		if (pointerSupported) {
-			accordionToggles[i].addEventListener("pointerdown", skipClickDelay, false);
-		}
-		accordionToggles[i].addEventListener("click", switchAccordion, false);
-	}
-})();
+VANTA.NET({
+  el: document.getElementById("animated_background"),
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  points: 14.0,
+  maxDistance: 22.0,
+  spacing: 16.0,
+  showDots: false,
+});
